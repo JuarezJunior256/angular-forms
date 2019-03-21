279d1f19e9f82ce5e7246f72e2e140bde42dd4d1
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-03-20 22:50:57 -0300

    Projeto em Andamento

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -15,9 +16,31 @@ export class TemplateFormComponent implements OnInit {
   onSubmit(form) {
     console.log(form);
   }
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'has-error': !campo.valid && campo.touched,
+      'has-feedback': !campo.valid && campo.touched
+    };
+  }
+
+  consultaCep(cep) {
+
+    cep = cep.replace(/\D/g, '');
+
+    if (cep !== '') {
+
+      const validaCep = /^[0-9]{8}$/;
+
+      if (validaCep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .subscribe(dados => console.log(dados));
+      }
+    }
   }
 
 }
