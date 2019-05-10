c9c1da615f9344954449576379ceca94893f4cfd
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-05-09 23:59:27 -0300

    Criando modulo e configurando para Formulário Reativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormModule } from './data-form/data-form.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-     DataFormComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     TemplateFormModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
