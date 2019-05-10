cfe4510c9ca4d836fd98e50e90564ad4a0d1a56c
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-05-10 00:46:04 -0300

    Criação do formulário reativo

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { log } from 'util';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +12,26 @@ export class DataFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
+
+    /* this.form = new FormGroup({
+      nome: new FormControl(),
+      email: new FormControl()
+    }); */
+
+    this.form = this.formBuilder.group({
+      nome: [null],
+      email: [null]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+
+    this.http.post('enderecoServer/formUser', JSON.stringify(this.form.value))
+       .subscribe(dados => console.log(dados));
   }
 
 }
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { DataFormComponent } from './data-form.component';
+import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule,
+    HttpClientModule
   ],
   declarations: [
     DataFormComponent
